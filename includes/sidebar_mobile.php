4c1bd37becf2572f0ab03042abf4fe3de266fd4a
Jazzel Ruiz <marcojazzelarzate@gmail.com>
2025-08-20 16:58:02 -0600

    cambios server2

--- includes/sidebar_mobile.php
+++ includes/sidebar_mobile.php
@@ -3,6 +3,7 @@
   <div class="offcanvas-header border-bottom">
     <h5 class="offcanvas-title">
       <img src="/sisec-ui/public/img/logoCESISS.jpeg" alt="Logo SISEC" style="max-width: 170px;">
+
     </h5>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Cerrar"></button>
   </div>
@@ -10,29 +11,17 @@
   <div class="offcanvas-body d-flex flex-column justify-content-between">
     <div style="font-size: 23px;"><?php if (in_array($_SESSION['usuario_rol'], ['Superadmin', 'Administrador', 'Mantenimientos', 'Técnico', 'Distrital'])): ?>
       <a href="/sisec-ui/views/inicio/index.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'inicio' ? 'active' : '' ?>"><i class="fas fa-home me-2"></i>Inicio</a>
-    <div style="font-size: 23px;"><?php if (in_array($_SESSION['usuario_rol'], ['Superadmin', 'Administrador', 'Mantenimientos', 'Técnico', 'Distrital'])): ?>
-      <a href="/sisec-ui/views/inicio/index.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'inicio' ? 'active' : '' ?>"><i class="fas fa-home me-2"></i>Inicio</a>
       <?php endif; ?>
       <a href="/sisec-ui/views/dispositivos/listar.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'dispositivos' ? 'active' : '' ?>"><i class="fas fa-camera me-2"></i>Dispositivos</a>
       <?php if (in_array($_SESSION['usuario_rol'], ['Superadmin','Administrador', 'Capturista','Técnico','Monitorista'])): ?>
         <a href="/sisec-ui/views/dispositivos/registro.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'registro' ? 'active' : '' ?>"><i class="fas fa-plus-circle me-2"></i>Registrar dispositivo</a>
-      <a href="/sisec-ui/views/dispositivos/listar.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'dispositivos' ? 'active' : '' ?>"><i class="fas fa-camera me-2"></i>Dispositivos</a>
-      <?php if (in_array($_SESSION['usuario_rol'], ['Superadmin','Administrador', 'Capturista','Técnico','Monitorista'])): ?>
-        <a href="/sisec-ui/views/dispositivos/registro.php" class="d-block mb-2 text-dark text-decoration-none <?= ($activePage ?? '') === 'registro' ? 'active' : '' ?>"><i class="fas fa-plus-circle me-2"></i>Registrar dispositivo</a>
       <?php endif; ?>
-      <?php if (in_array($_SESSION['usuario_rol'], ['Superadmin','Administrador'])): ?>
-        <a href="/sisec-ui/views/usuarios/index.php" class="d-block mb-2 text-dark text-decoration-none"><i class="fa-solid fa-users me-2"></i>Usuarios</a>
-        <a href="/sisec-ui/views/usuarios/registrar.php" class="d-block mb-2 text-dark text-decoration-none"><i class="fa-solid fa-user-plus me-2"></i>Registrar usuario</a>
       <?php if (in_array($_SESSION['usuario_rol'], ['Superadmin','Administrador'])): ?>
         <a href="/sisec-ui/views/usuarios/index.php" class="d-block mb-2 text-dark text-decoration-none"><i class="fa-solid fa-users me-2"></i>Usuarios</a>
         <a href="/sisec-ui/views/usuarios/registrar.php" class="d-block mb-2 text-dark text-decoration-none"><i class="fa-solid fa-user-plus me-2"></i>Registrar usuario</a>
       <?php endif; ?>
     </div>
     <?php if (isset($_SESSION['usuario_id'])): ?>
-    <div class="mt-3">
-      <hr/>
-      <a style="font-size:23px;" href="/sisec-ui/logout.php" class="d-block text-danger text-decoration-none"><i class="fas fa-sign-out-alt me-2"></i> Cerrar sesión</a>
-    </div>
     <div class="mt-3">
       <hr/>
       <a style="font-size:23px;" href="/sisec-ui/logout.php" class="d-block text-danger text-decoration-none"><i class="fas fa-sign-out-alt me-2"></i> Cerrar sesión</a>
